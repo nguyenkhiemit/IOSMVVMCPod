efd60376eb6ddcfd10646b74ce5d81ff0c1dae0c
Khiem Nguyen <khiem.d.nguyen@vntrip.vn>
2017-11-16 15:38:35 +0700

    Update

---
+++ IOSMVVMCPod/AppHelper.swift
@@ -0,0 +1,16 @@
+//
+//  AppHelper.swift
+//  IOSMVVMCPod
+//
+//  Created by Nguyen on 11/16/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AppHelper: NSObject {
+    
+    public func logHelper() {
+        print("This is your log")
+    }
+}
---
+++ Pods/Target Support Files/Pods-IOSMVVMCPod/Pods-IOSMVVMCPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOSMVVMCPod : NSObject
+@end
+@implementation PodsDummy_Pods_IOSMVVMCPod
+@end
